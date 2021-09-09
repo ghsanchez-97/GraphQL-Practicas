7b3a01cc06d4a8ecc212e8ab524572fd423ea9f1
Guy Sanchez <anakin25@live.com>
2021-09-09 14:55:32 -0600

    :construction: Instalación y configuración en graphql, y separación de los types, resolvers

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 'use strict'
 // Importación de GraphQL
-const { graphql, buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools')
 // Importación de Express
-const express = require('express');
+const express = require('express')
 // Importación de middleware GraphQL
 const { graphqlHTTP } = require('express-graphql')
+// Importación de otras librerías
+const { readFileSync } = require('fs')
+const { join } = require('path')
+//Importación de los resolvers
+const resolvers = require('./lib/resolvers')
 
 // Creación del servidor API
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 3000
 
-
-// Define the schema initial 
-const schema = buildSchema(`
-  type Query {
-      hello: String,
-      saludar: String
-  }
-`);
+// Creación del esquema GraphQL
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+// Define the schema initial
+const schema = makeExecutableSchema({typeDefs, resolvers})
 
 // Define the resolver
-const resolver = {
-    hello: () => {
-        return 'Hello World!';
-    },
-    saludar: () => {
-        return 'Hola a todos!';
-    }
-}
+//const resolver = resolvers
 
 // Ejecuar el query hello
 // graphql(schema, `{ saludar }`, resolver).then(result => {
 //     console.log(result);
-// }) 
+// })
 
 // Difinición del middleware GraphQL
 app.use('/api', graphqlHTTP({
-    schema: schema,
-    rootValue: resolver,
-    graphiql: true
-}));
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
 
 app.listen(port, () => {
-    console.log(`Server is listenig at http://localhost:${port}/api`)
-});
+  console.log(`Server is listenig at http://localhost:${port}/api`)
+})
